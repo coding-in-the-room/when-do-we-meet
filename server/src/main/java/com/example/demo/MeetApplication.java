d19930dc95ca68e02813ec2e069e63ef8a1dd6f8
riverandeye <riverandeye@gmail.com>
2020-05-17 00:29:23 +0900

    initialize server

--- server/src/main/java/com/example/demo/MeetApplication.java
+++ server/src/main/java/com/example/demo/MeetApplication.java
@@ -2,8 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MeetApplication {
 
 	public static void main(String[] args) {
@@ -11,4 +15,8 @@ public class MeetApplication {
 		SpringApplication.run(MeetApplication.class, args);
 	}
 
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value="name", defaultValue="World") String name){
+		return String.format("Hello %s", name);
+	}
 }
