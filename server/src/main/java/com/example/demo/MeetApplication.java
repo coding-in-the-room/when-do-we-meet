a5f4f8f7adc9743cf2b8c624f849571b9c379b88
riverandeye <riverandeye@gmail.com>
2020-05-16 11:50:21 +0900

    :rocket:

---
+++ server/src/main/java/com/example/demo/MeetApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		SpringApplication.run(MeetApplication.class, args);
+	}
+
+}
