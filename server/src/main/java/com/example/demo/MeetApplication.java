79cdd0eb463db976836acdb50eee317bb9322eca
riverandeye <riverandeye@gmail.com>
2020-06-04 02:12:03 +0900

    server | remove unnecessary mapping

--- server/src/main/java/com/example/demo/MeetApplication.java
+++ server/src/main/java/com/example/demo/MeetApplication.java
@@ -2,8 +2,6 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -11,12 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MeetApplication {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
 		SpringApplication.run(MeetApplication.class, args);
-	}
-
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value="name", defaultValue="World") String name){
-		return String.format("Hello %s", name);
 	}
 }
