09fe4427d827016dfb67630db1af61bccf0af1ee
riverandeye <riverandeye@gmail.com>
2020-06-05 01:37:36 +0900

    server | fix type of findById into Optional

--- server/src/main/java/com/example/demo/repository/UserRepository.java
+++ server/src/main/java/com/example/demo/repository/UserRepository.java
@@ -4,10 +4,11 @@ import com.example.demo.entity.User;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, Long> {
-  User findById(long Id);
 
+  Optional<User> findById(long Id);
   User findByName(String name);
 
   @Override
