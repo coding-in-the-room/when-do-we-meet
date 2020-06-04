dc5e13177f5e2653dd2d88da7dce3096ac24b245
riverandeye <riverandeye@gmail.com>
2020-06-05 01:37:58 +0900

    server | add implement of getUserById

--- server/src/main/java/com/example/demo/controller/UserController.java
+++ server/src/main/java/com/example/demo/controller/UserController.java
@@ -1,23 +1,35 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.UserResponse;
+import com.example.demo.dto.UsersResponse;
 import com.example.demo.entity.User;
-import com.example.demo.service.ScheduleService;
 import com.example.demo.service.UserService;
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("user")
 public class UserController {
+
   @Autowired
   private UserService userService;
 
   @GetMapping()
-  public List<User> getUser(){
-    return userService.getAllUsers();
+  public UsersResponse getAllUsers(){
+     List<User> users = userService.getAllUsers();
+     return new UsersResponse(users);
+  }
+
+  @GetMapping("/{id}")
+  public UserResponse getUserById(@PathVariable("id") long id) throws NotFoundException {
+    try {
+      User user = userService.getUserById(id);
+      return new UserResponse(user);
+    } catch (NotFoundException e){
+      throw new NotFoundException("이거 JSON으로 어캐 바꾸징ㅎㅎ");
+    }
   }
 }
