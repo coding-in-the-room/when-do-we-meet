070d920f3e1c05b6c5f84cc65efb9cb898d48a06
riverandeye <riverandeye@gmail.com>
2020-06-08 00:56:08 +0900

    client | add margin top for show content

--- client/src/component/layout/styles.tsx
+++ client/src/component/layout/styles.tsx
@@ -2,6 +2,8 @@ import styled from "styled-components/macro";
 
 export const Layout = styled.div``;
 
-export const Content = styled.div``;
+export const Content = styled.div`
+  margin-top: 5.6rem;
+`;
 
 export const ContentContainer = styled.div``;
