b3692b38e332dd2dada4f5195b325706399a6dc0
d4n3tt <danettofficial@gmail.com>
2020-06-12 00:04:31 +0900

    client | add color constant and logo

--- client/src/constant.ts
+++ client/src/constant.ts
@@ -2,5 +2,8 @@ export const BREAKPOINT = 768;
 
 export const THEME = {
   GRAY: "#E3E3E3",
-  PRIMARY: "#0066FF",
+  DARKGRAY: "#999999",
+  PRIMARY: "#BA78F3",
+  SECONDARY: "#F36D72",
+  TERTIARY: "#E9B677",
 };
